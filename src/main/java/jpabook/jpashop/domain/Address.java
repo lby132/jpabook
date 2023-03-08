bcebdf9b150dd53c3e339e3a3b8974ce91d8adb7
lby132 <quddbscjswow@naver.com>
2023-03-09 00:53:37 +0900

    basic

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+@Getter
+public class Address {
+
+    @Column(length = 10)
+    private String city;
+    @Column(length = 20)
+    private String street;
+    @Column(length = 5)
+    private String zipcode;
+
+    private String fullAddress() {
+        return getCity() + " " + getStreet() + " " + getZipcode();
+    }
+
+
+    //프록시를 자주 사용하는곳이라면 Objects.equals(getCity(), address.getCity()) 이런식으로 getter가 나오는 equals를 상속받는다.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) &&
+                Objects.equals(getStreet(), address.getStreet()) &&
+                Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
+    }
+}
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -13,9 +13,9 @@ public class Member extends BaseEntity {
     @Id @GeneratedValue
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
